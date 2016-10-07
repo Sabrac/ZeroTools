babf21e5ad1ba0e676f30825fb01010a2cdb372f
hongha1412 <hongha1412@gmail.com>
2016-10-07 07:00:06 +0000

    Add merge data tool

---
+++ MergeIniResource/FrmMergeData.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace MergeIniResource
+{
+    partial class FrmMergeData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.txtTargetFile = new System.Windows.Forms.TextBox();
+            this.txtSourceFile = new System.Windows.Forms.TextBox();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.emptySpaceItem2 = new DevExpress.XtraLayout.EmptySpaceItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.btnProcess);
+            this.layoutControl1.Controls.Add(this.txtLog);
+            this.layoutControl1.Controls.Add(this.txtTargetFile);
+            this.layoutControl1.Controls.Add(this.txtSourceFile);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(108, 96, 250, 350);
+            this.layoutControl1.OptionsPrint.AppearanceGroupCaption.BackColor = System.Drawing.Color.LightGray;
+            this.layoutControl1.OptionsPrint.AppearanceGroupCaption.Font = new System.Drawing.Font("Tahoma", 10.25F);
+            this.layoutControl1.OptionsPrint.AppearanceGroupCaption.Options.UseBackColor = true;
+            this.layoutControl1.OptionsPrint.AppearanceGroupCaption.Options.UseFont = true;
+            this.layoutControl1.OptionsPrint.AppearanceGroupCaption.Options.UseTextOptions = true;
+            this.layoutControl1.OptionsPrint.AppearanceGroupCaption.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.layoutControl1.OptionsPrint.AppearanceGroupCaption.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
+            this.layoutControl1.OptionsPrint.AppearanceItemCaption.Options.UseTextOptions = true;
+            this.layoutControl1.OptionsPrint.AppearanceItemCaption.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Near;
+            this.layoutControl1.OptionsPrint.AppearanceItemCaption.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Center;
+            this.layoutControl1.OptionsView.UseDefaultDragAndDropRendering = false;
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(629, 435);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 76);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(605, 323);
+            this.txtLog.TabIndex = 7;
+            this.txtLog.Text = "";
+            // 
+            // txtTargetFile
+            // 
+            this.txtTargetFile.Location = new System.Drawing.Point(67, 36);
+            this.txtTargetFile.Name = "txtTargetFile";
+            this.txtTargetFile.Size = new System.Drawing.Size(550, 20);
+            this.txtTargetFile.TabIndex = 6;
+            this.txtTargetFile.DragDrop += new System.Windows.Forms.DragEventHandler(this.txt_DragDrop);
+            this.txtTargetFile.DragEnter += new System.Windows.Forms.DragEventHandler(this.txt_DragEnter);
+            // 
+            // txtSourceFile
+            // 
+            this.txtSourceFile.Location = new System.Drawing.Point(67, 12);
+            this.txtSourceFile.Name = "txtSourceFile";
+            this.txtSourceFile.Size = new System.Drawing.Size(550, 20);
+            this.txtSourceFile.TabIndex = 5;
+            this.txtSourceFile.DragDrop += new System.Windows.Forms.DragEventHandler(this.txt_DragDrop);
+            this.txtSourceFile.DragEnter += new System.Windows.Forms.DragEventHandler(this.txt_DragEnter);
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.CustomizationFormText = "layoutControlGroup1";
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem2,
+            this.layoutControlItem1,
+            this.layoutControlItem3,
+            this.layoutControlItem4,
+            this.emptySpaceItem1,
+            this.emptySpaceItem2});
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "Root";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(629, 435);
+            this.layoutControlGroup1.Text = "Root";
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.txtSourceFile;
+            this.layoutControlItem2.CustomizationFormText = "Source File";
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(609, 24);
+            this.layoutControlItem2.Text = "Source File";
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(52, 13);
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.txtTargetFile;
+            this.layoutControlItem1.CustomizationFormText = "Target File";
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 24);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(609, 24);
+            this.layoutControlItem1.Text = "Target File";
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(52, 13);
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.txtLog;
+            this.layoutControlItem3.CustomizationFormText = "Log";
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 48);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(609, 343);
+            this.layoutControlItem3.Text = "Log";
+            this.layoutControlItem3.TextLocation = DevExpress.Utils.Locations.Top;
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(52, 13);
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(211, 403);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(209, 20);
+            this.btnProcess.TabIndex = 8;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.Control = this.btnProcess;
+            this.layoutControlItem4.CustomizationFormText = "layoutControlItem4";
+            this.layoutControlItem4.Location = new System.Drawing.Point(199, 391);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Size = new System.Drawing.Size(213, 24);
+            this.layoutControlItem4.Text = "layoutControlItem4";
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem4.TextVisible = false;
+            // 
+            // emptySpaceItem1
+            // 
+            this.emptySpaceItem1.AllowHotTrack = false;
+            this.emptySpaceItem1.CustomizationFormText = "emptySpaceItem1";
+            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 391);
+            this.emptySpaceItem1.Name = "emptySpaceItem1";
+            this.emptySpaceItem1.Size = new System.Drawing.Size(199, 24);
+            this.emptySpaceItem1.Text = "emptySpaceItem1";
+            this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // emptySpaceItem2
+            // 
+            this.emptySpaceItem2.AllowHotTrack = false;
+            this.emptySpaceItem2.CustomizationFormText = "emptySpaceItem2";
+            this.emptySpaceItem2.Location = new System.Drawing.Point(412, 391);
+            this.emptySpaceItem2.Name = "emptySpaceItem2";
+            this.emptySpaceItem2.Size = new System.Drawing.Size(197, 24);
+            this.emptySpaceItem2.Text = "emptySpaceItem2";
+            this.emptySpaceItem2.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // FrmMergeData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 435);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "FrmMergeData";
+            this.Text = "Merge Data Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private System.Windows.Forms.TextBox txtSourceFile;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private System.Windows.Forms.TextBox txtTargetFile;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private System.Windows.Forms.Button btnProcess;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem2;
+    }
+}
---
+++ MergeIniResource/FrmMergeData.cs
@@ -0,0 +1,239 @@
+﻿using MergeIniResource.EnumType;
+using MergeIniResource.Object;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MergeIniResource
+{
+    public partial class FrmMergeData : Form
+    {
+        BackgroundWorker bw;
+
+        public FrmMergeData()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            string s = "";
+
+            foreach (string File in FileList)
+            {
+                // s = s + " " + File;
+                s = File;
+            }
+
+            if (!s.Substring(s.LastIndexOf(".")).Trim().Equals("ini")) {
+                MessageBox.Show("File's format is invalid");
+                return;
+            }
+            ((TextBox)sender).Text = s;
+        }
+
+        private void txt_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            this.bw = new BackgroundWorker();
+            this.bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(this.Worker_RunWorkerCompleted);
+            this.bw.DoWork += new DoWorkEventHandler(this.Worker_DoWork);
+            this.bw.RunWorkerAsync();
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string source = txtSourceFile.Text;
+            string target = txtTargetFile.Text;
+            if (!source.Substring(source.LastIndexOf(".")).Trim().Equals("ini") ||
+                !target.Substring(target.LastIndexOf(".")).Trim().Equals("ini"))
+            {
+                MessageBox.Show("File's format is invalid");
+                return;
+            }
+
+            IniObject sourceIni = new IniObject(source);
+            IniObject targetIni = new IniObject(target);
+            sourceIni = ReadFile(sourceIni);
+            targetIni = ReadFile(targetIni);
+
+            foreach (KeyValuePair<String, GraphicObject> data in sourceIni.lsGraphic)
+            {
+                if (targetIni.lsGraphic.ContainsKey(data.Key))
+                {
+                    sourceIni.lsGraphic[data.Key] = targetIni.lsGraphic[data.Key];
+                    WriteLineLog("Found - " + data.Key);
+                }
+            }
+
+            List<IniObject> lsIni = new List<IniObject>();
+            lsIni.Add(sourceIni);
+            ExportFile(lsIni);
+
+            MessageBox.Show("Done !");
+        }
+
+        private void ExportFile(List<IniObject> lsIni)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (IniObject ini in lsIni)
+            {
+                foreach (GraphicObject graphic in ini.lsGraphic.Values)
+                {
+                    sb.Append(graphic.id).Append("=").Append(graphic.value).AppendLine();
+                }
+            }
+
+            if (sb.Length > 0)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    SaveFileDialog sfd = new SaveFileDialog();
+                    sfd.Filter = "Ini Files |*.ini|All Files |*.*";
+                    if (sfd.ShowDialog(this) == DialogResult.OK)
+                    {
+                        using (StreamWriter outfile = new StreamWriter(sfd.FileName))
+                        {
+                            outfile.Write(sb.ToString());
+                        }
+                    }
+                });
+            }
+            else
+            {
+                WriteLog("[Warning] ", Color.Orange);
+                WriteLineLog("Result string is null or empty.");
+            }
+        }
+
+        private IniObject ReadFile(IniObject iniFile)
+        {
+            string regexString = @"[0-9]+=[a-zA-Z0-9\/\\]+.(dds|c3)";
+            using (FileStream fs = File.Open(iniFile.name, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader sr = new StreamReader(bs))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (Regex.IsMatch(line, regexString, RegexOptions.IgnoreCase))
+                    {
+                        string[] part = line.Split('=');
+                        if (part.Length != 2)
+                        {
+                            WriteLog("[Error] ", Color.Red);
+                            WriteLineLog(String.Format("[File: {0}][Data: {1}] - Line does not contain 2 parts"));
+                        }
+                        else
+                        {
+                            GraphicObject graphic = new GraphicObject(part[0].ToString(), part[1].ToString());
+                            if (line.EndsWith("dds") || line.EndsWith("DDS"))
+                            {
+                                graphic.type = FileType.DDS;
+                            }
+                            else if (line.EndsWith("c3") || line.EndsWith("C3"))
+                            {
+                                graphic.type = FileType.C3;
+                            }
+                            else
+                            {
+                                graphic.type = FileType.OTHER;
+                            }
+
+                            if (iniFile.lsGraphic.ContainsKey(graphic.id))
+                            {
+                                string tmpId = graphic.id;
+                                int number = 1;
+                                while (iniFile.lsGraphic.ContainsKey(tmpId))
+                                {
+                                    tmpId = graphic.id + "_" + (number++);
+                                }
+                                iniFile.lsGraphic.Add(tmpId, graphic);
+                            }
+                            else
+                            {
+                                iniFile.lsGraphic.Add(graphic.id, graphic);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //WriteLog("[Warning] ", Color.Orange);
+                        //WriteLineLog(String.Format("[File: {0}][Data: {1}] - Line format not valid.", iniFile.name, line));
+                    }
+                }
+                return iniFile;
+            }
+        }
+
+        private string ReadFile(string file)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            using (FileStream fs = File.Open(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader sr = new StreamReader(bs))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    sb.AppendLine(line);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void WriteLineLog(string content, Color? textColor = null)
+        {
+            WriteLog(content, textColor);
+            WriteLog(Environment.NewLine);
+        }
+
+        private void WriteLog(string content, Color? textColor = null)
+        {
+            txtLog.Invoke(new Action(() =>
+            {
+                if (textColor == null)
+                {
+                    txtLog.AppendText(content);
+                }
+                else
+                {
+                    txtLog.SelectionStart = txtLog.TextLength;
+                    txtLog.SelectionLength = 0;
+
+                    txtLog.SelectionColor = (Color)textColor;
+                    txtLog.AppendText(content);
+                    txtLog.SelectionColor = txtLog.ForeColor;
+                }
+                txtLog.SelectionStart = txtLog.Text.Length;
+                txtLog.ScrollToCaret();
+            }
+            ));
+        }
+    }
+}
